581070d2399a17ed3f9c2dffda6ed41f7effed28
Shriteen <86562935+Shriteen@users.noreply.github.com>
2024-11-01 16:36:25 +0530

    Improved Client handling
    - Initialize the clients with random Id
    - Send refuse response to client before	closing	connection
    - More fields in SClient class along with their	getter/setters
    - send method in SClient to abstract sending messages
    - send ping to clients on connection

--- src/main/java/server/GameServer.java
+++ src/main/java/server/GameServer.java
@@ -2,13 +2,19 @@ package server;
 
 import java.net.Socket;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Random;
+
 import gamemodel.GameModel;
+import protocol.*;
 
 /**
  * The server of the game
  */
 public class GameServer extends Server {
+
+    private static final Random RNG= new Random();
 
     // Maximum number of players allowed to connect
     int maxPlayers = 8;
@@ -26,25 +32,41 @@ public class GameServer extends Server {
 
     // methods which handles each incoming connection
     void clientHandler(Socket sock) {
-        
-        // If count exceeds maxCount then reject new clients 
-        if(clientList.size() < maxPlayers)
-            clientList.add(new SClient(sock));
-        else{
-            //TODO: send refuse response to client
-            System.out.println("Refusing connection to "+ sock +" as count exceeds "+maxPlayers);
-            
-            try {
+        try {
+            // If count exceeds maxCount then reject new clients 
+            if(clientList.size() < maxPlayers)
+                clientList.add(new SClient(sock, this, getUniqueRandomId()));
+            else{
+                System.out.println("Refusing connection to "+ sock +" as count exceeds "+maxPlayers);
+
+                //send refused message
+                (new PrintWriter(sock.getOutputStream(), true)).println(
+                    new ErrorMessage(ErrorMessage.Code.CONN_REFUSED,
+                                     "Max player limit reached!"
+                        ).toString()
+                    );
+                
                 sock.close();
             }
-            catch (IOException e) {
-                System.out.println("Error " + e.getMessage());
-                e.printStackTrace();
-            }
+        }
+        catch (IOException e) {
+            System.out.println("Error " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
     public void setMaxPlayers(int count) {
         maxPlayers= count;
+    }
+
+    // Returns a random integer which is not already allotted to any client
+    private int getUniqueRandomId() {
+        int randomValue = RNG.nextInt(1000);
+
+        // 0 is used as magic number and is invalid id
+        if(randomValue==0 || clientList.stream().anyMatch(sc -> sc.getClientId() == randomValue))
+            return getUniqueRandomId();                // if not valid just try again
+
+        return randomValue;
     }
 }
--- src/main/java/server/SClient.java
+++ src/main/java/server/SClient.java
@@ -1,6 +1,10 @@
 package server;
 
 import java.net.Socket;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+import protocol.*;
 
 /**
  * SClient represents a client on the server side
@@ -9,16 +13,65 @@ public class SClient extends Thread{
 
     // The socket connecting to client
     private Socket socket;
+    // Output stream to write to socket
+    private PrintWriter writer;
 
+    // Reference to game server
+    private GameServer server;
+
+    // Represents whether client is actively connected 
+    private boolean active;
+
+    // Client info
+    private final int clientId;
+    private String username;
+    
     // initialise socket and start communication in a new thread
-    SClient(Socket socket) {
+    SClient(Socket socket, GameServer server, int id) throws IOException{
         this.socket = socket;
-
+        this.server = server;
+        active = true;
+        clientId=id;
+        username = String.valueOf(id);
+        
+        writer = new PrintWriter(socket.getOutputStream(), true);
         start();
     }
 
     public void run() {
         //TODO: Actual implementation
-        System.out.println("connected to "+socket);
+        System.out.println("connected to "+socket+ " Client ID:"+clientId+ " Username:"+username);
+
+        try {
+            //Send ping as test for communication
+            send(new Ping());
+        }
+        catch (Throwable e) {
+            System.out.println("Error " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        
+    }
+
+    public void send(Message message) throws IOException {
+        writer.println(message.toString());
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    // set active to false i.e. do not consider client to be useful anymore. Like a close()
+    public void deactivate() {
+        active = false;
     }
 }
