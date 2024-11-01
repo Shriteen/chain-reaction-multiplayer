72b60bacb619dd9e501856dd859ea40a1ac69893
Shriteen <86562935+Shriteen@users.noreply.github.com>
2024-11-01 21:01:13 +0530

    Sclient receiver loop
    - Receive Loop and basic message handling for SClient including error display, exit, client hello
    - Send server hello on connect

--- src/main/java/server/SClient.java
+++ src/main/java/server/SClient.java
@@ -1,8 +1,8 @@
 package server;
 
 import java.net.Socket;
-import java.io.PrintWriter;
-import java.io.IOException;
+import java.io.*;
+import com.google.gson.Gson;
 
 import protocol.*;
 
@@ -13,7 +13,8 @@ public class SClient extends Thread{
 
     // The socket connecting to client
     private Socket socket;
-    // Output stream to write to socket
+    // Input Output streams to read/write from/to socket
+    private BufferedReader reader;
     private PrintWriter writer;
 
     // Reference to game server
@@ -35,25 +36,96 @@ public class SClient extends Thread{
         username = String.valueOf(id);
         
         writer = new PrintWriter(socket.getOutputStream(), true);
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        
         start();
     }
 
     public void run() {
-        //TODO: Actual implementation
         System.out.println("connected to "+socket+ " Client ID:"+clientId+ " Username:"+username);
 
         try {
-            //Send ping as test for communication
-            send(new Ping());
+            //send hello to client to convey its id
+            send(new ServerHello(clientId));
+            
+            while (active) {
+                try {
+                    //Send ping as test for communication,
+                    //will give error in case other side is closed
+                    send(new Ping());
+                    if(writer.checkError()){
+                        active=false;
+                        continue;                    
+                    }
+                    
+                    String jsonMessage=reader.readLine();
+                    if(jsonMessage==null)
+                        continue;
+
+                    Message message=new Gson().fromJson(jsonMessage, Message.class);
+
+                    // The heart of thread which handles incoming messages
+                    // Handle generic connection related messages
+                    switch(message.messageType){
+                        
+                    case "PING":
+                        System.out.println("Received ping from "+clientId);
+                        break;
+                    case "ERROR_MESSAGE":
+                        ErrorMessage errorMessage= new Gson().fromJson(jsonMessage, ErrorMessage.class);
+                        System.out.println(errorMessage.getErrorDisplayString());
+                        break;
+                    case "EXIT":
+                        active=false;
+                        break;
+                        
+                    // The messages specific to game will be passed to handler method
+                    // Note we need to list down all the game specific messages here to pass them
+                    case "CLIENT_HELLO":
+                        handleMessages(message.messageType, jsonMessage);
+                        break;
+                        
+                    default:
+                        System.out.println("Received unknown message type: "+message.messageType);
+                    }
+                    
+                }
+                catch (IOException e) {
+                    active=false;
+                }
+            }
         }
         catch (Throwable e) {
             System.out.println("Error " + e.getMessage());
             e.printStackTrace();
+        } finally {
+            
+            try {
+                // Close socket after use
+                socket.close();
+                System.out.println("Closed socket; clientId: "+clientId);
+            }
+            catch (Throwable e) {
+                System.out.println("Error " + e.getMessage());
+                e.printStackTrace();
+            }
         }
-
-        
     }
 
+    // handle game related messages 
+    private void handleMessages(String messageType, String jsonMessage) {
+        // Handle game related messages;
+        // Do not forget to update switch-cases in run() so that this method is called!
+        switch(messageType){
+            
+        case "CLIENT_HELLO":
+            ClientHello message= new Gson().fromJson(jsonMessage, ClientHello.class);
+            username= message.userName;
+            System.out.println("Client "+clientId+" updated their username to "+username);
+            break;
+        }
+    }
+    
     public void send(Message message) throws IOException {
         writer.println(message.toString());
     }
