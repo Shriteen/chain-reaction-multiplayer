6f6230777851a98e078d029701841e082f11e2f9
Shriteen <86562935+Shriteen@users.noreply.github.com>
2024-11-03 22:32:47 +0530

    GameServer updates
    - CleanDaemon thread which removes inactive clients and clients who exceed max player limit
      from clientList along with handling of removing from game
    - setMaxPlayers can only be used in LISTENING state
    - getter for gameModel
    - basic implementation on GameLoop thread (Just creates thread and prints game state; Implementation remaining)
    - startGame method which initializes the game model and starts game loop thread
    - method to get hex colors used to initialize game model with player list

--- src/main/java/server/GameServer.java
+++ src/main/java/server/GameServer.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import gamemodel.GameModel;
+import gamemodel.Player;
 import protocol.*;
 
 /**
@@ -28,6 +29,10 @@ public class GameServer extends Server {
     public GameServer() throws IOException{
         super(45678);
         clientList= new ArrayList<SClient>();
+
+
+        // Thread to clean inactive clients
+        new CleanDaemon();
     }
 
     // methods which handles each incoming connection
@@ -55,10 +60,18 @@ public class GameServer extends Server {
         }
     }
 
-    public void setMaxPlayers(int count) {
-        maxPlayers= count;
+    //Changing maxPlayers is allowed only in LISTENING state
+    public void setMaxPlayers(int count) throws IllegalStateException{
+        if(state==Server.State.LISTENING)
+            maxPlayers= count;
+        else
+            throw new IllegalStateException("Changing max players is allowed only during server is in LISTENING state!");
     }
 
+    public GameModel getGameState() {
+        return model;
+    }
+    
     // Returns a random integer which is not already allotted to any client
     private int getUniqueRandomId() {
         int randomValue = RNG.nextInt(1000);
@@ -69,4 +82,120 @@ public class GameServer extends Server {
 
         return randomValue;
     }
+
+    // Start the game
+    public void startGame(int rows, int cols) {
+
+        ArrayList<Player> players=new ArrayList<Player>();
+
+        synchronized(clientList){
+            for (int i=0; i<clientList.size() && i<maxPlayers; i++) {
+                if(clientList.get(i).isActive()){
+                    players.add(new Player(
+                                    clientList.get(i).getClientId(),
+                                    clientList.get(i).getUsername() ,
+                                    getColorForIndex(i)
+                                    ) );
+                }
+            }
+        }
+        
+        model = new GameModel(rows, cols, players );
+
+        //start loop thread
+        new GameLoop();
+    }
+    
+    // Daemon thread which removes inactive clients from clientList
+    private class CleanDaemon extends Thread {
+        CleanDaemon() {
+            this.start();
+        }
+        
+        public void run() {
+            // Loop every 1 seconds
+            while(state!= Server.State.STOPPED){
+                try {
+                    Thread.sleep(1000);
+                    
+                    synchronized(clientList)
+                    {
+                        // If game has started, also need to remove from game
+                        if(state == Server.State.STARTED) {
+                            for(SClient sc : clientList) {
+                                if(!sc.isActive()){
+                                    model.removePlayer(sc.getClientId());
+                                }
+                            }
+                        }
+                        
+                        clientList.removeIf(sc-> !sc.isActive());
+                    }
+
+                    // Remove players if exceeding latest max players limit even after removing inactive
+                    if(clientList.size() > maxPlayers){
+                        synchronized(clientList){
+                            for(int i=maxPlayers; i<clientList.size(); i++) {                                
+                                try {
+                                    clientList.get(i).send(
+                                        new ErrorMessage(ErrorMessage.Code.CONN_REFUSED,
+                                                         "Max player limit changed" ) );
+                                }
+                                catch (IOException e) {
+                                    System.out.println("Error " + e.getMessage());
+                                    e.printStackTrace();
+                                }
+                                
+                                clientList.get(i).deactivate();
+                            }
+                            clientList.subList(maxPlayers,clientList.size()).clear();
+                        }
+                    }
+                    
+                }
+                catch (InterruptedException e) {
+                    System.out.println("CleanDaemon interrupted");
+                }
+            }
+        }   
+    }
+
+    // Thread for loop in which game is played
+    private class GameLoop extends Thread {
+        GameLoop() {
+            state= Server.State.STARTED;
+            this.start();
+        }
+        
+        public void run() {
+            // TODO: game loop implementation
+            System.out.println("game is "+state.name());
+            model.print();
+        }   
+    }
+    
+    // Utility method to returns a hex color prioritizing basic colors
+    static private String getColorForIndex(int i) {
+        final String[] colors= { "FFFF00", "00FFFF", "FF00FF",
+                                 "FF0000", "00FF00", "0000FF",
+                                 "FFFFFF", "FFA500" };
+        if (i<colors.length) {
+            return colors[i];
+        }else{
+            //Generate random color
+            int red,green,blue;
+            while(true){
+                red = RNG.nextInt(255);
+                green= RNG.nextInt(255);
+                blue= RNG.nextInt(255);
+
+                //should be bright
+                if((red+green+blue)/3 > 127)
+                    break;
+            }
+
+            return Integer.toHexString(red)+Integer.toHexString(green)+Integer.toHexString(blue);
+        }
+    }
+    
 }
