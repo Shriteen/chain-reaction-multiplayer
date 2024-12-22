204943d9eb3096fee38fe3bf77f9c2901e31d8dd
Shriteen <86562935+Shriteen@users.noreply.github.com>
2024-12-22 16:26:31 +0530

    Lobby Screens
    - Basic client and server lobby screens with working controls and validations
    - Transition from Home to lobby screens
    - Utility class to format input

---
+++ src/main/java/ui/ClientLobbyController.java
@@ -0,0 +1,20 @@
+package ui;
+
+import java.util.ResourceBundle;
+import java.net.URL;
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+
+import static app.App.server;
+import static app.App.client;
+
+/**
+ * ClientLobby: The screen which waits till server starts game
+ */
+public class ClientLobbyController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+}
