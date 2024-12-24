b9864fbb39451ef1d6208e83fb229f1acfe557ec
Shriteen <86562935+Shriteen@users.noreply.github.com>
2024-12-24 22:18:48 +0530

    Board Screen
    - Basic board screen with controller
    - Transition from server and client lobby to board screen
    - Basic handling of new state to render it
    - Exit button to stop game and navigate to home page

--- src/main/java/ui/ClientLobbyController.java
+++ src/main/java/ui/ClientLobbyController.java
@@ -4,6 +4,12 @@ import java.util.ResourceBundle;
 import java.net.URL;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.application.Platform;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 
 import static app.App.server;
 import static app.App.client;
@@ -13,8 +19,30 @@ import static app.App.client;
  */
 public class ClientLobbyController implements Initializable {
     
+    @FXML
+    private VBox container;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        client.onGameStateReceived(model->{
+                Platform.runLater(()->{
+                        try {
+                            
+                            URL boardFxml=getClass().getResource("/fxml/board.fxml");
+                            Parent root= FXMLLoader.load(boardFxml);
+                            ((Stage)container.getScene().getWindow()).setScene(
+                                new Scene(root,640, 480)
+                                );
+                    
+                        }
+                        catch (Throwable e) {
+                            System.out.println("Error " + e.getMessage());
+                            e.printStackTrace();
+                        }   
+                    });
+            });
+
+        //refresh game state
+        client.refreshStateRequest();
     }    
 }
